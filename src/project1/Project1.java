a43b345cdbe2cb6c89808b31c46733088da7a172
Eric Peterson <eric.peterson100@gmail.com>
2011-09-03 17:55:41 -0600

    started cs236 repository for Fall 2011 Semester

---
+++ src/project1/Project1.java
@@ -0,0 +1,15 @@
+//REQUIRED CLASS - KEEP THIS FILE IN THE 'project1' DIRECTORY WITH 'package project1'
+//IMPLEMENT THE 'body' METHOD. MAKE AN INSTANCE OF 'LexicalAnalyzer' and return an ouput string
+//DO NOT ADD METHODS
+
+package project1;
+
+public class Project1
+{
+	public static String body(String[] args)
+	{
+System.out.println("Implement the Project1.body");
+		String output = "";
+		return output;
+	}
+}
---
+++ src/project1/Token.java
@@ -0,0 +1,6 @@
+//REQUIRED CLASS
+package project1;
+
+public class Token
+{
+}

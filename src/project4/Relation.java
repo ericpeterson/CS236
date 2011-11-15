2d7d9ed844a13b8d32e5b48183fa7e802a706443
Eric Peterson <eric.peterson100@gmail.com>
2011-11-14 19:04:25 -0700

    Started Project 4

--- src/project4/Relation.java
+++ src/project4/Relation.java
@@ -4,6 +4,8 @@ package project4;
 import project1.*;
 import project2.*;
 
-public class Relation
-{
+public class Relation {
+  private void rename() {}
+  private void select() {} 
+  private void project() {}
 }
